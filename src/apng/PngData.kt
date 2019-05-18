27ca333e3c5d810773d8347c151ad8f33dc9ac8e
K <kamiyaowl@gmail.com>
2019-05-18 22:40:44 +0900

    読み出し周りをリファクタリング

--- src/apng/PngData.kt
+++ src/apng/PngData.kt
@@ -3,8 +3,18 @@ package apng
 import java.io.File
 import java.io.InputStream
 
-val ByteArray.toBigEndian get() = {
-    (this[0].toInt() shl 24) or (this[1].toInt() shl 16)or (this[2].toInt() shl 8) or this[3].toInt()
+val ByteArray.toIntArray get() = {
+    val len = this.size / 4
+    val list = mutableListOf<Int>()
+    for(i in 0 until len) {
+        val ptr = i * 4
+        val data = (this[ptr + 0].toInt() shl 24) or (this[ptr + 1].toInt() shl 16) or (this[ptr + 2].toInt() shl 8) or (this[ptr + 3].toInt() shl 0)
+        list.add(data)
+    }
+    list.toIntArray()
+}
+val Collection<Byte>.toIntArray get() = {
+    this.toByteArray().toIntArray()
 }
 
 class PngData {
@@ -13,21 +23,10 @@ class PngData {
         // Result, ErrorMessage
         fun read(path: String): PngData {
             val stream: InputStream = File(path).inputStream()
-            fun readByte(byteLen: Int) : ByteArray {
-                val buf = ByteArray(byteLen)
+            fun readByte(len: Int) : ByteArray {
+                val buf = ByteArray(len)
                 stream.read(buf, 0, buf.size)
                 return buf
-            }
-            fun readInt(len: Int) : IntArray {
-                val list = mutableListOf<Int>()
-                val buf = ByteArray(len * 4)
-                stream.read(buf, 0, buf.size)
-                for(i in 0 until len) {
-                    val ptr = i * 4
-                    val data = (buf[ptr + 0].toInt() shl 24) or (buf[ptr + 1].toInt() shl 16) or (buf[ptr + 2].toInt() shl 8) or (buf[ptr + 3].toInt() shl 0)
-                    list.add(data)
-                }
-                return list.toIntArray()
             }
             val dst = PngData()
             val sig = readByte(SIGNATURE.size)
@@ -35,10 +34,13 @@ class PngData {
                 throw IllegalArgumentException("PNGファイルではありません")
             }
             while(stream.available() > 0) {
-                val (byteLen) = readInt(1)
-                val chunkType = readByte(4).map { it.toChar() }.joinToString(separator = "")
-                val chunkData = readInt(byteLen / 4)
-                val (crc) = readInt(1)
+                val (dataLen) = readByte(4).toIntArray()
+                val readLen = 4 + dataLen + 1 // chunktype(1) + chunkdata + crc(1)
+                val data = readByte(readLen)
+
+                val chunkTypeStr = data.take(4).joinToString("") { "${it.toChar()}" }
+                val chunkData = data.drop(4).take(dataLen).toIntArray()
+                val crc = data.last() // chunkType + chunkDataで計算
             }
             // チャンクデータの読み出し
             stream.close()
