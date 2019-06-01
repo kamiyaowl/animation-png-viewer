57b08ea6fdbe277d6a1689a62996e35a45149115
K <kamiyaowl@gmail.com>
2019-06-01 23:59:25 +0900

    header lengthがよめた

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"os"
@@ -8,21 +9,39 @@ import (
 )
 
 // TODO: PNGをちゃんと型にしてあげる
-func parsePng(f *os.File) (png []byte, err error) {
+func parsePng(f *os.File) (png []uint8, err error) {
 	// png header check
-	validSignature := []byte{0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a}
-	signature := make([]byte, len(validSignature))
+	validSignature := []uint8{0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a}
+	signature := make([]uint8, len(validSignature))
 	n, err := f.Read(signature)
 	if n == 0 {
-		return nil, errors.New("png header read error")
+		return nil, errors.New("png headerが読み取れなかった")
 	}
 	if err != nil {
 		return nil, err
 	}
 	if !reflect.DeepEqual(validSignature, signature) {
-		return nil, errors.New("This file is not png format.")
+		return nil, errors.New("pngファイルではない")
 	}
-
+	// read chunks
+	for {
+		// length(4uint8) + chunk-type
+		headersBuf := make([]uint8, 8)
+		n, err := f.Read(headersBuf)
+		if n < len(headersBuf) {
+			// EOF
+			break
+		}
+		if err != nil {
+			return nil, errors.New("Chunkヘッダの読み込みエラー")
+		}
+		length := binary.BigEndian.Uint32(headersBuf[0:4])
+		chunkType := string(headersBuf[4:4])
+		fmt.Println(length)
+		fmt.Println(chunkType)
+		// Test
+		break
+	}
 	return nil, nil
 }
 func main() {
